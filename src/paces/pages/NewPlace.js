015e67010e647bd9d6c6b8ffa187dfaab7eec113
Habakuk <habakuk.conrado@gmail.com>
2023-03-27 11:26:38 -0300

    class59 finished

--- src/paces/pages/NewPlace.js
+++ src/paces/pages/NewPlace.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
+
 function NewPlace() {
-  return <div>NewPlace</div>;
+  return (
+    <form className="place-form">
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+        validators={[]}
+        errorText="Please, enter a valid title."
+      />
+    </form>
+  );
 }
 
 export default NewPlace;
