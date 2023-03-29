8554d568b1d19f4d7be7c523a07c450f1438289e
Habakuk <habakuk.conrado@gmail.com>
2023-03-28 23:07:22 -0300

    class81 finished

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const fs = require("fs");
+
 const userName = "Habakuk";
 
-console.log(userName);
+//alert(userName); //Not accessible API
+
+fs.writeFile("user-data.txt", "Name: " + userName, (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("File is ready!");
+  }
+});
